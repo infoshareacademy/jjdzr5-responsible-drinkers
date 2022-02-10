7fd34207609156ad8737379db51ff21e419ae55f
Tomek Jaworski <tj.toja@gmai.com>
2022-02-10 14:10:37 +0100

    Added variables, getters & setters in files Drink.java & Drinks.java

--- src/main/java/com/infoshareacademy/Drinks.java
+++ src/main/java/com/infoshareacademy/Drinks.java
@@ -1,4 +1,15 @@
 package com.infoshareacademy;
 
+import java.util.List;
+
 public class Drinks {
+    private List<Drink> drinks;
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
 }
