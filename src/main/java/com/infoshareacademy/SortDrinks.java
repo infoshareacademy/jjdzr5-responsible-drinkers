3e0d6750ef9763f7593af842ff31fe9b22ec43b8
Tomek Jaworski <tj.toja@gmai.com>
2022-02-17 08:00:02 +0100

    Added methods in SortDrinks.java

--- src/main/java/com/infoshareacademy/SortDrinks.java
+++ src/main/java/com/infoshareacademy/SortDrinks.java
@@ -2,8 +2,27 @@ package com.infoshareacademy;
 
 import org.apache.log4j.Logger;
 
+import java.util.List;
+
 public class SortDrinks {
 
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
+    private List<Drink> drinkList;
+
+    public SortDrinks(List<Drink> drinkList) {
+        this.drinkList = drinkList;
+    }
+
+    public List<Drink> getSortedByName() {
+        return null;
+    }
+
+    public List<Drink> getSortedByID() {
+        return null;
+    }
+
+    public List<Drink> getSortedByAlcohol() {
+        return null;
+    }
 }
