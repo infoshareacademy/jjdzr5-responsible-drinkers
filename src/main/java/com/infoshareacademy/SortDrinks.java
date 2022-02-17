c6c4eb6eb5655a18aa590cb27b976c37ba5e7f86
Tomek Jaworski <tj.toja@gmai.com>
2022-02-17 09:56:51 +0100

    Added public method to return result

--- src/main/java/com/infoshareacademy/SortDrinks.java
+++ src/main/java/com/infoshareacademy/SortDrinks.java
@@ -2,27 +2,58 @@ package com.infoshareacademy;
 
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortDrinks {
 
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
-    private List<Drink> drinkList;
+    private final List<Drink> drinkList;
 
     public SortDrinks(List<Drink> drinkList) {
         this.drinkList = drinkList;
     }
 
-    public List<Drink> getSortedByName() {
-        return null;
+    private List<Drink> getSortedByName(boolean isASC) {
+        if (isASC) {
+            return drinkList.stream().sorted(new DrinkNameComparator()).collect(Collectors.toList());
+        }
+        return drinkList.stream().sorted(new DrinkNameComparator().reversed()).collect(Collectors.toList());
     }
 
-    public List<Drink> getSortedByID() {
-        return null;
+    private List<Drink> getSortedByID(boolean isASC) {
+        if (isASC) {
+            return drinkList.stream().sorted(new DrinkIDComparator()).collect(Collectors.toList());
+        }
+        return drinkList.stream().sorted(new DrinkIDComparator().reversed()).collect(Collectors.toList());
     }
 
-    public List<Drink> getSortedByAlcohol() {
-        return null;
+    private List<Drink> getSortedByAlcoholic(boolean isASC) {
+        if (isASC) {
+            return drinkList.stream().sorted(new DrinkAlcoholicComparator()).collect(Collectors.toList());
+        }
+        return drinkList.stream().sorted(new DrinkAlcoholicComparator().reversed()).collect(Collectors.toList());
+    }
+
+    public List<Drink> getSortedList(SortItems item, boolean isASC) {
+        List<Drink> result = new ArrayList<>();
+
+        switch (item) {
+            case ID: {
+                result = getSortedByID(isASC);
+                break;
+            }
+            case ALCOHOLIC: {
+                result = getSortedByAlcoholic(isASC);
+                break;
+            }
+            case DRINK_NAME: {
+                result = getSortedByName(isASC);
+                break;
+            }
+        }
+        return result;
     }
 }
