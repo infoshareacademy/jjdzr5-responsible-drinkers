fb4266ee4d4ae04639df742c1cb5d4f597f7db59
Tomek Jaworski <tj.toja@gmai.com>
2022-02-21 08:24:45 +0100

    Added empty methods in DrinkManager.java

--- src/main/java/com/infoshareacademy/DrinkManager.java
+++ src/main/java/com/infoshareacademy/DrinkManager.java
@@ -1,4 +1,32 @@
 package com.infoshareacademy;
 
+import java.util.List;
+
 public class DrinkManager {
+
+    private final List<Drink> drinkList;
+
+    public DrinkManager(List<Drink> drinkList) {
+        this.drinkList = drinkList;
+    }
+
+    public List<Drink> addDrink(Drink newDrink) {
+        return null;
+    }
+
+    public List<Drink> removeDrink(int byIndex) {
+        return null;
+    }
+
+    public List<Drink> removeDrink(Drink drink) {
+        return null;
+    }
+
+    public List<Drink> modifyDrink(Drink drink) {
+        return null;
+    }
+
+    public List<Drink> modifyDrink(int byIndex) {
+        return null;
+    }
 }
