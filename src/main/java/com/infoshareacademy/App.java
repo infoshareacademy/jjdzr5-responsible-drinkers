1960d520801c75118816b205cfcc08c52af14343
Tomek Jaworski <tj.toja@gmai.com>
2022-02-23 09:11:46 +0100

    Added new static class to read properties

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -9,12 +9,12 @@ public class App {
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
-        LOGGER.log(Level.INFO,"Responsible drinkers");
+        LOGGER.log(Level.INFO, "Responsible drinkers");
         System.out.println(ApplicationProperties.getDatePattern());
         System.out.println(ApplicationProperties.getSortDirection());
-        //        new AppProperties().printProperties();
-        System.out.println(new SortDrinks(new JsonReader().getDrinkList()).getSortedList(SortItems.ID,true));
-     //   Menu menu = new Menu();
-     //   menu.run();
+        new AppProperties().printProperties();
+        System.out.println(new SortDrinks(new JsonReader().getDrinkList()).getSortedList(SortItems.ID, true));
+        //   Menu menu = new Menu();
+        //   menu.run();
     }
 }
