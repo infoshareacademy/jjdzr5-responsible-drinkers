bb4f96d99e6cf94e8e5b3b1176ad58f8d2e63a13
Tomek Jaworski <tj.toja@gmai.com>
2022-03-03 10:26:22 +0100

    Refactor files

--- src/main/java/com/infoshareacademy/drinkers/service/sorting/DrinkAlcoholicComparator.java
+++ src/main/java/com/infoshareacademy/drinkers/service/sorting/DrinkAlcoholicComparator.java
@@ -1,13 +1,13 @@
-package com.infoshareacademy.drinkers.service.filtering;
+package com.infoshareacademy.drinkers.service.sorting;
 
 import com.infoshareacademy.drinkers.domain.drink.Drink;
 
 import java.util.Comparator;
 
-public class DrinkDateModifiedComparator implements Comparator<Drink> {
+public class DrinkAlcoholicComparator implements Comparator<Drink> {
     @Override
     public int compare(Drink o1, Drink o2) {
-        return o1.getDateModified().compareTo(o2.getDateModified());
+        return o1.getAlcoholic().compareTo(o2.getAlcoholic());
     }
 
     @Override
