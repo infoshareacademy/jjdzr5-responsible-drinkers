b006d57ecf2c00d8de3bb50c5f5fd54d6fd8972f
Tomek Jaworski <tj.toja@gmai.com>
2022-03-03 13:33:10 +0100

    Implemented static methods

--- src/main/java/com/infoshareacademy/drinkers/service/properties/ApplicationProperties.java
+++ src/main/java/com/infoshareacademy/drinkers/service/properties/ApplicationProperties.java
@@ -6,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,8 +15,9 @@ import java.util.logging.Logger;
 public class ApplicationProperties {
 
     private static final Logger LOGGER = Logger.getLogger(ApplicationProperties.class.getName());
-
-    private static final Path PATH_FILE_NAME = Paths.get("src", "main", "resources","app_01.properties");
+    private static final Path PATH_FILE_NAME = Paths.get("src", "main", "resources", "app_01.properties");
+    private static final String ASC = "ASC";
+    private static final String DESC = "DESC";
 
     public static String getDatePattern() {
         Properties properties = readFileProperties(PATH_FILE_NAME);
@@ -60,5 +63,31 @@ public class ApplicationProperties {
         return appProperties;
     }
 
+    public static DateFormat getDataFormatPattern() {
+        String datePatternFromProperties = getDatePattern();
+        DateFormat format = new SimpleDateFormat();
+        try {
+            format = new SimpleDateFormat(datePatternFromProperties);
+        } catch (NullPointerException e) {
+            LOGGER.log(Level.INFO, "Date patter is null", e);
+        } catch (IllegalArgumentException e) {
+            LOGGER.log(Level.INFO, "Date patter error", e);
+        }
+        return format;
+    }
 
+    public static boolean isAscendingSort() {
+        if (getSortDirection().equalsIgnoreCase(DESC)) {
+            return false;
+        } else if (getSortDirection().equalsIgnoreCase(ASC)) {
+            return true;
+        } else {
+            try {
+                throw new Exception("Cant find parameter");
+            } catch (Exception e) {
+                LOGGER.log(Level.INFO, "Exception reading ASC/DESC Properties. Default: ascending sorting", e);
+                return true;
+            }
+        }
+    }
 }
