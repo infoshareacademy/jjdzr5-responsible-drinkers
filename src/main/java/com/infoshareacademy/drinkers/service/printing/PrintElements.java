86a998a2ce1f9ad77b77af2afc0958d22a0e8dc6
Tomek Jaworski <tj.toja@gmai.com>
2022-03-04 08:58:09 +0100

    Implemented printing method

--- src/main/java/com/infoshareacademy/drinkers/service/printing/PrintElements.java
+++ src/main/java/com/infoshareacademy/drinkers/service/printing/PrintElements.java
@@ -1,6 +1,7 @@
 package com.infoshareacademy.drinkers.service.printing;
 
 import com.infoshareacademy.drinkers.domain.drink.Drink;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -13,12 +14,37 @@ public class PrintElements {
     }
 
     public void print() {
+        System.out.print(printFrameHorizontal());
         for (Drink drink : drinkList) {
             printElement(drink);
         }
+        System.out.print(printFrameHorizontal());
+
     }
 
     private void printElement(Drink drink) {
+        StringBuilder lineBuilder = new StringBuilder();
+        lineBuilder
+                .append("|")
+                .append(StringUtils.center(String.format("%d ", drink.getIdDrink()),9))
+                .append("|")
+                .append(StringUtils.center(String.format("%s ", drink.getDrink()),16))
+                .append("|")
+                .append(StringUtils.center(String.format("%s ", drink.getAlcoholic()),16))
+                .append("|")
+                .append(StringUtils.center(String.format("%s ", drink.getGlass()),23))
+                .append("|")
+                .append(System.lineSeparator());
+        System.out.print(lineBuilder);
+    }
 
+    private String printFrameHorizontal() {
+        int width =85;
+        StringBuilder result = new StringBuilder("+");
+        for (int i=0;i<width;i++) {
+                result.append("-");
+        }
+        result.append("+").append(System.lineSeparator());
+        return result.toString();
     }
 }
