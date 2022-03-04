de4728d5018d3626fb3b653fdb6528c780e60266
Tomek Jaworski <tj.toja@gmai.com>
2022-03-04 09:47:48 +0100

    Implemented printing method

--- src/main/java/com/infoshareacademy/drinkers/service/printing/PrintElements.java
+++ src/main/java/com/infoshareacademy/drinkers/service/printing/PrintElements.java
@@ -4,8 +4,12 @@ import com.infoshareacademy.drinkers.domain.drink.Drink;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class PrintElements {
+
+    private static final Logger LOGGER = Logger.getLogger(PrintElements.class.getName());
+    private static final int LINE_LENGTH = 145;
 
     private final List<Drink> drinkList;
 
@@ -15,36 +19,46 @@ public class PrintElements {
 
     public void print() {
         System.out.print(printFrameHorizontal());
+        int index = 0;
         for (Drink drink : drinkList) {
-            printElement(drink);
+            printElement(drink, index);
+            index++;
         }
         System.out.print(printFrameHorizontal());
 
     }
 
-    private void printElement(Drink drink) {
+    private void printElement(Drink drink, int index) {
+
+        String strIngredients = "";
+        for (String s : drink.getIngredients()) {
+            if (strIngredients.equals("")) {
+                strIngredients = s;
+            } else {
+                strIngredients += ", " + s;
+            }
+        }
+
         StringBuilder lineBuilder = new StringBuilder();
         lineBuilder
                 .append("|")
-                .append(StringUtils.center(String.format("%d ", drink.getIdDrink()),9))
+                .append(StringUtils.center(String.format("%d.", index + 1), 6)).append("|")
+                .append(StringUtils.center(String.format("%d", drink.getIdDrink()), 10))
                 .append("|")
-                .append(StringUtils.center(String.format("%s ", drink.getDrink()),16))
+                .append(StringUtils.center(String.format("%s", drink.getDrink()), 16))
                 .append("|")
-                .append(StringUtils.center(String.format("%s ", drink.getAlcoholic()),16))
+                .append(StringUtils.center(String.format("%s", drink.getAlcoholic()), 16))
                 .append("|")
-                .append(StringUtils.center(String.format("%s ", drink.getGlass()),23))
+                .append(StringUtils.center(String.format("%s", drink.getGlass()), 22))
+                .append("|")
+                .append(StringUtils.left(String.format(" %-70s", strIngredients), 70))
                 .append("|")
                 .append(System.lineSeparator());
         System.out.print(lineBuilder);
     }
 
     private String printFrameHorizontal() {
-        int width =85;
-        StringBuilder result = new StringBuilder("+");
-        for (int i=0;i<width;i++) {
-                result.append("-");
-        }
-        result.append("+").append(System.lineSeparator());
-        return result.toString();
+        return "+" + "-".repeat(LINE_LENGTH) + "+" + System.lineSeparator();
+
     }
 }
