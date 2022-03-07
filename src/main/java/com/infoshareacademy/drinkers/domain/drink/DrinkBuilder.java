1cd80c339d74860e311504b26cbceab2248fffde
Tomek Jaworski <tj.toja@gmai.com>
2022-03-07 10:46:41 +0100

    Implemented builder

--- src/main/java/com/infoshareacademy/drinkers/domain/drink/DrinkBuilder.java
+++ src/main/java/com/infoshareacademy/drinkers/domain/drink/DrinkBuilder.java
@@ -2,8 +2,12 @@ package com.infoshareacademy.drinkers.domain.drink;
 
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class DrinkBuilder {
+public final class DrinkBuilder {
+
+    private static final Logger LOGGER = Logger.getLogger(DrinkBuilder.class.getName());
 
     private int idDrink;
     private String strDrink;
@@ -59,6 +63,80 @@ public class DrinkBuilder {
 
     public Drink build() {
         Drink drink = new Drink();
+        if (idDrink <= 0) {
+            throw new IllegalStateException("ID cannot be empty");
+        }
+        if (strDrink.isEmpty()) {
+            throw new IllegalStateException("Name cannot be empty");
+        }
+        drink.setIdDrink(idDrink);
+        drink.setDrinkName(strDrink);
+        drink.setDrinkAlternate(strDrinkAlternate);
+        drink.setDateModified(LocalDateTime.now());
         return drink;
+    }
+
+    public DrinkBuilder setID(Integer ID) {
+        this.idDrink = ID;
+        return this;
+    }
+
+    public DrinkBuilder setName(String drinkName) {
+        this.strDrink = drinkName;
+        return this;
+    }
+
+    public DrinkBuilder setDrinkAlternate(String drinkAlternate) {
+        this.strDrinkAlternate = drinkAlternate;
+        return this;
+    }
+
+    public DrinkBuilder setisAlcoholic(boolean isAlcoholic) {
+        if (isAlcoholic) {
+            this.strAlcoholic = Alcoholic.ALCOHOLIC.getName();
+            return this;
+        } else {
+            this.strAlcoholic = Alcoholic.NON_ALCOHOLIC.getName();
+            return this;
+        }
+    }
+
+    public DrinkBuilder setIngredients(String... ingredient) {
+        if (ingredient.length < 9) {
+            if (!ingredient[0].isEmpty()) {
+                this.strIngredient1 = ingredient[0];
+            }
+            if (!ingredient[1].isEmpty()) {
+                this.strIngredient2 = ingredient[1];
+            }
+            if (!ingredient[2].isEmpty()) {
+                this.strIngredient3 = ingredient[2];
+            }
+            if (!ingredient[3].isEmpty()) {
+                this.strIngredient4 = ingredient[3];
+            }
+            if (!ingredient[4].isEmpty()) {
+                this.strIngredient5 = ingredient[4];
+            }
+            if (!ingredient[5].isEmpty()) {
+                this.strIngredient6 = ingredient[5];
+            }
+            if (!ingredient[6].isEmpty()) {
+                this.strIngredient7 = ingredient[6];
+            }
+            if (!ingredient[7].isEmpty()) {
+                this.strIngredient8 = ingredient[7];
+            }
+            if (!ingredient[8].isEmpty()) {
+                this.strIngredient9 = ingredient[8];
+            }
+            if (!ingredient[9].isEmpty()) {
+                this.strIngredient10 = ingredient[9];
+            }
+            return this;
+        } else {
+            LOGGER.log(Level.INFO, "Max 10 ingredients");
+            return this;
+        }
     }
 }
