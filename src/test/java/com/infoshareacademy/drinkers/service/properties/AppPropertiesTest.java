c9e2737a20d2c77cbfc9819469ea47605e02bd1f
Marek Maciejewski <marekmaciejewski1985@gmail.com>
2022-05-14 18:01:06 +0200

    parameterized test show off

--- src/test/java/com/infoshareacademy/drinkers/service/properties/AppPropertiesTest.java
+++ src/test/java/com/infoshareacademy/drinkers/service/properties/AppPropertiesTest.java
@@ -2,13 +2,17 @@ package com.infoshareacademy.drinkers.service.properties;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +48,18 @@ class AppPropertiesTest {
     void getDatePattern_returnsNonEmptyProperty() {
         // given
         String expectedDatePattern = "non empty";
+        given(properties.getProperty("dateFormat")).willReturn(expectedDatePattern);
+        // when
+        String actualDatePattern = appProperties.getDatePattern();
+        // then
+        assertSame(expectedDatePattern, actualDatePattern);
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {"non empty", "some other value"})
+    void getDatePattern_returnsNonEmptyProperty(String expectedDatePattern) {
+        // given
         given(properties.getProperty("dateFormat")).willReturn(expectedDatePattern);
         // when
         String actualDatePattern = appProperties.getDatePattern();
